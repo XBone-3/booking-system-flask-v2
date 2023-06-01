bf1fb544081146a8b76241e993c81053373b66f8
XBone-3 <gtavc.wasted@gmail.com>
2023-06-01 09:44:56 +0530

    removed QUERY

--- statics.py
+++ statics.py
@@ -1,6 +1,5 @@
 import os
 
-
 MONTHS = {	
             1:'January',
             2:'February',
@@ -20,10 +19,11 @@ COURT_1 = 'Court 1'
 COURT_2 = 'Court 2'
 CRICKET = 'Cricket'
 BADMINTON = 'Badminton'
-SLOTS = ['6:00 am', '8:00 am', '5:30 pm', '8:30 pm']
+B_SLOTS = ['6:00 am', '8:00 am', '5:30 pm', '8:30 pm']
 C_SLOTS = ['forenoon', 'afternoon', 'night']
 PASSWORD = 'fool'.encode()
 SECRET_KEY = os.urandom(32)
+SUCCESSFULL_LOGIN_MESSAGE = "Successfully Logged"
 
 BADMINTON_CHOICES = [("6:00 am", "slot 1"), ("8:00 am", "slot 2"), ("5:30 pm", "slot 3"), ("8:30 pm", "slot 4")]
 CRICKET_CHOICES = [("forenoon", "forenoon"), ("afternoon", "afternoon"), ("night", "night")]
@@ -41,18 +41,3 @@ HTMLS = {
     'admin login': 'admin_login.html',
     'about': 'About.html',
 }
-
-QUERY = '''INSERT INTO slotsavailable 
-            (sport, courtname, availability, timeslot)
-            VALUES 
-            ("Cricket", "Cricket", 1, "forenoon"),
-            ("Cricket", "Cricket", 1, "afternoon"),
-            ("Cricket", "Cricket", 1, "night"),
-            ("Badminton", "Court 1", 1, "6:00 am"),
-            ("Badminton", "Court 1", 1, "8:00 am"),
-            ("Badminton", "Court 1", 1, "5:30 pm"),
-            ("Badminton", "Court 1", 1, "8:30 pm"),
-            ("Badminton", "Court 2", 1, "6:00 am"),
-            ("Badminton", "Court 2", 1, "8:00 am"),
-            ("Badminton", "Court 2", 1, "5:30 pm"),
-            ("Badminton", "Court 2", 1, "8:30 pm");'''
